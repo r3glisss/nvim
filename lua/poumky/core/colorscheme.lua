e3348e43bec07a7376bfc1f98731ee06d4df4287
Poumky <you@example.com>
2022-11-15 11:25:54 +0100

    first commit

---
+++ lua/poumky/core/colorscheme.lua
@@ -0,0 +1,9 @@
+-- local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+-- if not status then
+--   return
+-- end
+-- Lua
+require('onedark').setup {
+  style = 'deep'
+}
+require('onedark').load()
