631ddba008707d0b6c29994f4b594d408ae54f9d
R3glisss <cyrbresch@gmail.com>
2022-11-27 18:32:34 +0100

    Toggle term configuration powershell

--- lua/poumky/plugins/toggleterm.lua
+++ lua/poumky/plugins/toggleterm.lua
@@ -4,6 +4,19 @@ if not toggleterm_status then
   return
 end
 
+local powershell_options = {
+  shell = vim.fn.executable "pwsh" and "pwsh" or "powershell",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
+
 toggleterm.setup({
   size = 20,
   open_mapping = [[<leader>ù]],
